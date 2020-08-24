5e4ca1dc6c1e98621f24f8a3a2e35156975abc9d
haiqiwu <haiqiwu@yahoo.com>
2020-08-23 22:31:24 -0400

    favicon 2

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const compression = require('compression');
 const path = require('path');
 const app = express();
-require('./public/favicon.ico');
  
 app.use(compression());
 app.use(express.static(path.join(__dirname, 'build')));
